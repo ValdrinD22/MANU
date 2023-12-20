045c8b9e764ee26fecf28d0b3c907601bd5ee478
ValdrinD20 <valdrindalloshi@gmail.com>
2023-12-21 00:57:24 +0100

    12

---
+++ WebApi1/Auth/AuthResults.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApi1.Auth
+{
+    public class AuthResults
+    {
+        public string? Token { get; set; }
+        public bool Result { get; set; }
+        public List<String>? Errors { get; set; }
+    }
+}
